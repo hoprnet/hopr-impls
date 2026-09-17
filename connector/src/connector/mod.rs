8b431c0359360c22caa7996c6e1ce5d4888bd4bb
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2026-09-17 14:51:14 +0200

    feat: set the `custom_tx_multiplier` for channel-related txs to 4 (#24)

--- connector/src/connector/mod.rs
+++ connector/src/connector/mod.rs
@@ -56,6 +56,7 @@ const REGISTRY_READ_BUDGET_DIVISOR: u32 = 4;
 const MIN_TX_CONFIRM_TIMEOUT: Duration = Duration::from_secs(1);
 const TX_TIMEOUT_MULTIPLIER: u32 = 2;
 const DEFAULT_SYNC_TOLERANCE_PCT: usize = 90;
+const CHANNEL_CUSTOM_TIMEMOUT_MULTIPLIER: u32 = 4;
 
 /// Connector health states.
 ///
